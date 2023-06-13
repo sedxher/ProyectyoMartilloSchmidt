0d58dd291972df964e7265b57f54efede2e319e5
sedxher <sedxher@gmail.com>
2023-06-13 00:55:09 -0400

    Actualizado a grid

--- main.py
+++ main.py
@@ -1,77 +1,83 @@
-import tkinter as tk
+import tkinter
+from tkinter import ttk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
-class InterfazGrafica:
-    def __init__(self, master):
-        self.master = master
-        master.title("UCS a partir de Schmidt")
-        
-        # Crear entrada de texto en la parte superior
-        self.entrada = tk.Entry(master)
-        self.entrada.grid(column=0, row=0)
+modelos = ["Modelo1","Modelo2"]
 
-        # Crear menú desplegable
-        self.menu_desplegable = tk.Menubutton(master, text="Opciones")
-        self.menu_desplegable.menu = tk.Menu(self.menu_desplegable, tearoff=0)
-        self.menu_desplegable["menu"] = self.menu_desplegable.menu
-        self.menu_desplegable.menu.add_command(label="Opción 1", command=self.opcion1)
-        self.menu_desplegable.menu.add_command(label="Opción 2", command=self.opcion2)
-        self.menu_desplegable.menu.add_command(label="Opción 3", command=self.opcion3)
-        self.menu_desplegable.grid(column=1, row=1)
+## FUNCIONES
 
-        # Crear botones en la parte superior
-        self.button1 = tk.Button(master, text="Archivo", command=self.abrir_dialogo)
-        self.button1.grid(column=0, row=1)
-        self.button2 = tk.Button(master, text="Calcular", command=self.plot_grafico)
-        self.button2.grid(column=0, row=2)
-
-        # Crear figura de Matplotlib
-        self.figura = Figure(figsize=(5, 5), dpi=100)
-        self.grafico = self.figura.add_subplot(111)
-        self.canvas = FigureCanvasTkAgg(self.figura, master=master)
-        self.canvas.get_tk_widget().grid(column=2, row=2)
-        
-        # Crear botones en la parte superior
-        self.button3 = tk.Button(master, text="Guardar", command=self.guardar)
-        self.button3.grid(column=3, row=3)
-
-    def plot_grafico(self):
+def plotter():
         # Generar grafica
         x = [1, 2, 3, 4, 5]
         y = [2, 3, 1, 4, 2]
-        self.grafico.plot(x, y)
-        self.canvas.draw()
+        grafico.plot(x, y)
+        canvas.draw()
 
-    def limpiar_grafico(self):
-        # Limpiar grafico de Matplotlib
-        self.grafico.clear()
-        self.canvas.draw()
-        
-    def opcion1(self):
-        self.menu_desplegable.config(text="Modelo 1")
+def abrir_dialogo():
+        carpeta = tkinter.filedialog.askdirectory()
 
-    def opcion2(self):
-        self.menu_desplegable.config(text="Modelo 2")
+def mostrar_ventana():
+        tkinter.messagebox.showinfo("Mensaje", "Introduzca un nombre para su proyecto")
 
-    def opcion3(self):
-        self.menu_desplegable.config(text="Modelo 3")
-        
-    def abrir_dialogo(self):
-        # Abrir el diálogo de selección de archivo
-        carpeta = tk.filedialog.askdirectory()
-    
-    def guardar(self):
-        texto_nombre = self.entrada.get()
+def guardar():
+        texto_nombre = entrada_nombre_proyecto.get()
         if texto_nombre == '':
-            self.mostrar_ventana()
+            mostrar_ventana()
         else:
             print(texto_nombre)
-            
-    def mostrar_ventana(self):
-        # Mostrar un mensaje en la ventana emergente
-        tk.messagebox.showinfo("Mensaje", "Introduzca un nombre para su proyecto")
 
-root = tk.Tk()
-mi_interfaz = InterfazGrafica(root)
-root.mainloop()
+ventana = tkinter.Tk()
+#ventana.geometry('600x400')
+ventana.title("USC a partir de Martillo Schmidt")
+
+## MARCO INFORMACION
+frame = tkinter.Frame(ventana)
+frame.pack()
+informacion_modelo =tkinter.LabelFrame(frame, text="Información del modelo")
+informacion_modelo.grid(row= 0, column=0, sticky="news", padx=40, pady=20)
+
+# Nombre del proyecto
+nombre_proyecto = tkinter.Label(informacion_modelo, text="Nombre del proyecto")
+nombre_proyecto.grid(row=0, column=0, columnspan=5)
+entrada_nombre_proyecto = tkinter.Entry(informacion_modelo)
+entrada_nombre_proyecto.grid(row=1, column=0, columnspan=5)
+
+# Modo
+mode_label = tkinter.Label(informacion_modelo, text="Modo")
+mode_combobox = ttk.Combobox(informacion_modelo, values=modelos)
+mode_label.grid(row=4, column=0)
+mode_combobox.grid(row=5, column=0)
+
+# Modelo
+model_label = tkinter.Label(informacion_modelo, text="Modelo")
+model_combobox = ttk.Combobox(informacion_modelo, values=modelos)
+model_label.grid(row=6, column=0)
+model_combobox.grid(row=7, column=0)
+
+# Abrir
+open_button = tkinter.Button(informacion_modelo, text="Abrir", command=abrir_dialogo)
+open_button.grid(row=4, column=2, sticky="news", padx=20, pady=10)
+
+## MARCO FIGURA
+figure_frame = tkinter.LabelFrame(frame)
+figure_frame.grid(row=1, column=0, sticky="news", padx=20, pady=5)
+
+# Crear figura de Matplotlib
+figura = Figure(figsize=(4, 4), dpi=100)
+grafico = figura.add_subplot(111)
+canvas = FigureCanvasTkAgg(figura, master=figure_frame)
+canvas.get_tk_widget().grid(row=0, column=0,rowspan=3, columnspan=3, sticky="news")
+
+## MARCO ACCIONES
+buttons_frame = tkinter.LabelFrame(frame)
+buttons_frame.grid(row=2, column=0, sticky="news", padx=20, pady=10)
+
+
+run_button = tkinter.Button(buttons_frame, text="Calcular", command=plotter)
+run_button.grid(row=3, column=0, sticky="news", padx=20, pady=10)
+
+save_button = tkinter.Button(buttons_frame, text="Guardar", command=guardar)
+save_button.grid(row=3, column=1, sticky="news", padx=20, pady=10)
+
+ventana.mainloop()
